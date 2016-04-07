58f32ad2b515f09d78748d9bb1d4a646f3ab7682
Ferryvanderlaak <ferry.vanderlaak@student.fontys.nl>
2016-04-07 11:47:20 +0200

    meer test

--- CAREMATCH/CAREMATCH/Login/Start.cs
+++ CAREMATCH/CAREMATCH/Login/Start.cs
@@ -15,6 +15,7 @@ namespace Login
     public partial class Start : Form
     {        
         private Loginn login;
+        
         public Start()
         {
             InitializeComponent();
@@ -25,25 +26,34 @@ namespace Login
         //Logincheck
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            HomeForm homeForm = new HomeForm();
+            DateTime vandaag = new DateTime();
+            Image piet = CAREMATCH.Properties.Resources.users;
+            HomeForm homeForm;
+            Gebruiker gebruiker;
             //login.LoginCheck(textBox1.Text, textBox2.Text, this);
             if (textBox1.Text == "beheerder" && textBox2.Text == "beheerder")
-            {          
+            {
+                gebruiker = new Gebruiker("henk", "piet", "piet", piet, "piet", vandaag, true);
+                homeForm = new HomeForm(gebruiker);
+                
                 homeForm.Show();
                 this.Hide();
                 
             }
             else if (textBox1.Text == "vrijwilliger" && textBox2.Text == "beheerder")
             {
+                gebruiker = new Gebruiker("henk", "piet", "piet", piet, "piet", vandaag, true);
+                homeForm = new HomeForm(gebruiker);
                 homeForm.Show();
                 this.Hide();
                 
             }
             else if (textBox1.Text == "hulpbehoevende" && textBox2.Text == "hulpbehoevende")
             {
+                gebruiker = new Gebruiker("henk", "piet", "piet", piet, "piet", vandaag, true);
+                homeForm = new HomeForm(gebruiker);
                 homeForm.Show();
-                this.Hide();
-                
+                this.Hide(); 
             }
         }
 

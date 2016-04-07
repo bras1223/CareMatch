73fd9fe6ea044e77ba2bb4a4087cf1887365c66b
Yannic Riesebos <zebybez@surfspot.nu>
2016-04-07 14:48:01 +0200

    added comment

--- CAREMATCH/CAREMATCH/VrijwilligerSysteem/HulpvraagOverzichtForm.cs
+++ CAREMATCH/CAREMATCH/VrijwilligerSysteem/HulpvraagOverzichtForm.cs
@@ -36,7 +36,7 @@ namespace CAREMATCH.VrijwilligerSysteem
                 lblGebruikersnaam.Text = gebruiker.Gebruikersnaam;
                 lblGebruikerType.Text = "beheerder";
             }
-
+            hulpvraagList
         }
 
         private void lbHulpvragen_SelectedIndexChanged(object sender, EventArgs e)

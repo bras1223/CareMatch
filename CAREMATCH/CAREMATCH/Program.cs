0058a9814e8c6d3d2bdb95fa773acec7caa1f5f1
Ferryvanderlaak <ferry.vanderlaak@student.fontys.nl>
2016-04-07 11:03:47 +0200

    Login changes

--- CAREMATCH/CAREMATCH/Program.cs
+++ CAREMATCH/CAREMATCH/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CAREMATCH.VrijwilligerSysteem;
+using Login;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace CAREMATCH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomeForm());
+            Application.Run(new Start());
         }
     }
 }

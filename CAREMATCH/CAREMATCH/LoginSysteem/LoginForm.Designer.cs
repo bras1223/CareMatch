1179b26c3f6b825f90986a186811859d98321920
Mark van Lierop <mark.vanlierop@student.fontys.nl>
2016-04-14 15:29:31 +0200

    login aangepast

--- CAREMATCH/CAREMATCH/LoginSysteem/LoginForm.Designer.cs
+++ CAREMATCH/CAREMATCH/LoginSysteem/LoginForm.Designer.cs
@@ -44,7 +44,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(22, 156);
             this.label2.Name = "label2";
@@ -55,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(22, 89);
             this.label1.Name = "label1";

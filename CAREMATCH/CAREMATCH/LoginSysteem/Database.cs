e8413e7f05bad81d86d8deb89bb56f722e7ebd9b
Yannic Riesebos <zebybez@surfspot.nu>
2016-04-28 15:05:10 +0200

    approved

--- CAREMATCH/CAREMATCH/LoginSysteem/Database.cs
+++ CAREMATCH/CAREMATCH/LoginSysteem/Database.cs
@@ -736,12 +736,17 @@ namespace CAREMATCH
                     gebruiker = new Gebruiker();
                     if (reader["ROL"].ToString().ToLower() == "vrijwilliger")
                     {
-                        //Kan niet vergelijken met string & char. Database approved column moet naar varchar2. alle gebruikers eerst verwijderen.
-                        //if(reader["Approved"].ToString() == 'Y')
-                        //{
+                        //Kan niet vergelijken met string &char.Database approved column moet naar varchar2.alle gebruikers eerst verwijderen.
+                        if (reader["Approved"].ToString() == "Y")
+                        {
+                            gebruiker.Approved = true;
 
-                        //}
-                        gebruiker.Approved = true;
+                        }
+                        else
+                        {
+                            gebruiker.Approved = false;
+                        }
+
                     }
                     //Properties toekennen aan gebruiken.
                     gebruiker.Achternaam = reader["Achternaam"].ToString();
